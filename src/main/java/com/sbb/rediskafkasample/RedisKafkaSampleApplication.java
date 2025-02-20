befbedd70a8ca71bfe054ce7d7ef53d1d377a950
seohyeokgyu <shk64099@gmail.com>
2025-02-20 22:31:51 +0900

    initial commit

---
+++ src/main/java/com/sbb/rediskafkasample/RedisKafkaSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sbb.rediskafkasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisKafkaSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisKafkaSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbb/rediskafkasample/RedisKafkaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbb.rediskafkasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisKafkaSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
