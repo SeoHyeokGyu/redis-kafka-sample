20c35e6f4f6941f8c234a8138c0281676268c264
seohyeokgyu <shk64099@gmail.com>
2025-02-20 22:57:29 +0900

    initail commit

--- src/main/java/com/sbb/rediskafkasample/RedisKafkaSampleApplication.java
+++ src/main/java/com/sbb/rediskafkasample/RedisKafkaSampleApplication.java
@@ -1,13 +1,18 @@
 package com.sbb.rediskafkasample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedisKafkaSampleApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(RedisKafkaSampleApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(RedisKafkaSampleApplication.class, args);
+		log.debug("test");
 	}
 
 }
