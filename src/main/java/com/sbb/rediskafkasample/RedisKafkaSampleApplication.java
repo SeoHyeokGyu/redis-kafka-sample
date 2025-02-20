5131d9b4144d7706a551e98268b268168a449ac6
seohyeokgyu <shk64099@gmail.com>
2025-02-20 22:52:20 +0900

    initail commit

--- src/main/java/com/sbb/rediskafkasample/RedisKafkaSampleApplication.java
+++ src/main/java/com/sbb/rediskafkasample/RedisKafkaSampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RedisKafkaSampleApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RedisKafkaSampleApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RedisKafkaSampleApplication.class, args);
+	}
 
 }
