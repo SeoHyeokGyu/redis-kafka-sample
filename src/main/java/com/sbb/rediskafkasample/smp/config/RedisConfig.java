f4e77431bf55eb00cb06621b165956f4c1faa006
seohyeokgyu <shk64099@gmail.com>
2025-02-23 01:50:44 +0900

    RedisConfig commit

--- src/main/java/com/sbb/rediskafkasample/smp/config/RedisConfig.java
+++ src/main/java/com/sbb/rediskafkasample/smp/config/RedisConfig.java
@@ -1,16 +1,30 @@
 package com.sbb.rediskafkasample.smp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-
 public class RedisConfig {
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // Redis 연결 설정 (호스트, 포트 등)
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName("localhost");  // Redis 호스트
+        configuration.setPort(6379);            // Redis 포트
+        configuration.setPassword("temp");  // Redis 비밀번호 (비밀번호가 있다면 설정)
+        return new LettuceConnectionFactory(configuration);  // Lettuce는 기본 Redis 클라이언트
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
+        template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
